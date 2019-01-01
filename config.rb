39384922e8acc3b8abd22e8272663e5bb819e76a
Oleksandr Tsukur <tsukuros@gmail.com>
2019-01-01 22:05:55 +0200

    add hero section on home page

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+set :name, 'Oleks Consulting'
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
